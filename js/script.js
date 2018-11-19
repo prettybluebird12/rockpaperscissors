5863d483c7792ab5f0455c9561360b0323dff71a
Savannah Milton <sjamz5555@gmail.com>
2018-11-19 23:11:56 +0000

    if else

--- js/script.js
+++ js/script.js
@@ -8,4 +8,25 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+$("#shoot").click(function(){
+  var message = $("#shoot").val();
+  $("#userchoice").text(message);
+  
+    userChoice = $("#input").val();
+  $("#userchoice").text(userChoice);
+  if(randomNumber > 0 && randomNumber<.40){
+   $("#computerchoice").text("rock"); 
+  }
+  else if(randomNumber > .40 && randomNumber < .60){
+    $("#computerchoice").text("paper");
+  }
+  else if(randomNumber> .60 && randomNumber< .99){
+    $("#computerchoice").text("scissors");
+  }
 
+});
+
+var userChoice = "";
+var computerChoice= "";
+var winner = "";
+var randomNumber = math.Random;
