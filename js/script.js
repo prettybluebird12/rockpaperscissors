525a83811beacc41bd3055caed2e921576941bc4
Savannah Milton <sjamz5555@gmail.com>
2018-12-02 22:36:11 +0000

    finished

--- js/script.js
+++ js/script.js
@@ -8,22 +8,30 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+var userChoice = "";
+var computerChoice= "";
+var winner = "";
+
+
 $("#shoot").click(function(){
+  var randomNumber =Math.random();
   var message = $("#shoot").val();
   $("#userchoice").text(message);
   
     userChoice = $("#input").val();
   $("#userChoice").text(userChoice);
   
-  
   if(randomNumber > 0 && randomNumber<.40){
    $("#computerChoice").text("rock"); 
+   computerChoice = "rock";
   }
   else if(randomNumber > .40 && randomNumber < .60){
     $("#computerChoice").text("paper");
+    computerChoice = "paper";
   }
   else if(randomNumber> .60 && randomNumber< .99){
     $("#computerChoice").text("scissors");
+    computerChoice = "scissors";
   }
 
 if(userChoice === computerChoice){
@@ -36,6 +44,9 @@ else if(userChoice === "rock" && computerChoice === "scissors"){
   
 else if(userChoice === "scissors" && computerChoice === "paper"){
   $("#result").text("User Wins");}
+  
+  else if(userChoice === "scissors" && computerChoice === "rock"){
+  $("#result").text("Computer Wins");}
 
 else if(userChoice === "paper" && computerChoice === "scissors"){
   $("#result").text("Computer Wins");}
@@ -50,7 +61,3 @@ $("#input").val("");
 $("#computerChoice").val("");
 });
 
-var userChoice = "";
-var computerChoice= "";
-var winner = "";
-var randomNumber =Math.random();
